ec3da004afd4dfe32e6df185a17969da78a811cb
Scott Bailey <scott@zeddic.com>
2018-10-20 23:25:29 -0700

    Adds boilerplate for two client routes.
    This CL adds the boilerplate for a browser and
    game room page. Sets up routing for the apps.
    A followup change will move the current game
    into the game page.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,19 +10,27 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 import {MatButtonModule} from '@angular/material/button';
 import {SharedModule} from './shared/shared.module';
 import {GameStore} from './shared/stores/game_store';
+import { GameRoomComponent } from './game-room/game-room.component';
+import { BrowserComponent } from './browser/browser.component';
+import {RoutesModule} from './routes-module';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
+    GameRoomComponent,
+    BrowserComponent,
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
-    FormsModule,
+    BrowserAnimationsModule,
+    BrowserModule,
     DragDropModule,
+    FormsModule,
     MatButtonModule,
+    RoutesModule,
+    RouterModule,
     SharedModule,
   ],
   providers: [GameStore],
---
+++ client/src/app/routes-module.ts
@@ -0,0 +1,21 @@
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
+import {BrowserComponent} from "./browser/browser.component";
+import {GameRoomComponent} from "./game-room/game-room.component";
+
+const APP_ROUTES: Routes = [
+  {path: 'browser', component: BrowserComponent},
+  {path: 'game/:id', component: GameRoomComponent},
+  {
+    path: '',
+    redirectTo: '/browser',
+    pathMatch: 'full'
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(APP_ROUTES)
+  ],
+})
+export class RoutesModule {}
